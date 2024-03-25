ac12c3a6d886eac351e3e45c7980e0db29b1db09
Victor Castañeda <115094771+victor-Casta@users.noreply.github.com>
2024-03-25 18:19:56 -0500

    🔥update: segunda parte de ejercicios pt-2

--- logica-de-programacion/exercises/exercise2/python/exercise.py
+++ logica-de-programacion/exercises/exercise2/python/exercise.py
@@ -21,7 +21,7 @@ def user_numbers():
 
 def pair_numbers():
   counter = 2
-  while counter <= 100:
+  while counter <= 10:
     print(counter)
     counter += 2
 
@@ -41,6 +41,13 @@ def factorial():
     counter += 1
   print(f'el factorial de {user_number} es igual a: {factorial}')
 
+def multiply_numbers_three():
+  counter = 1
+  while counter <= 100:
+    if counter % 3 == 0:
+      print(counter)
+    counter += 1
+
 def addition_digits():
   user_number = int(input('Ingresa un numero: '))
   suma = 0
@@ -49,16 +56,35 @@ def addition_digits():
     user_number //= 10
   print(f'la suma de los digitos de {user_number} es igual a: {suma}')
 
+def odd_numbers_to_user_number():
+  user_number = int(input('Ingresa un numero: '))
+  counter = 1
+  while counter <= user_number:
+    if counter % 2!= 0:
+      print(counter)
+    counter += 1
+
+def pair_number_to_one_hundred():
+  addition = 0
+  counter = 0
+  while counter <= 100:
+    print(counter)
+    addition += counter
+    counter += 2
+  print(f'la suma de los 100 primeros numeros naturales es igual a: {addition}')
 
 while True:
   print('Selecciona una opcion: ')
   print('1 -> Numeros del 1 al 10')
   print('2 -> Suma de los 100 primeros numeros naturales')
   print('3 -> Numeros de 1 hasta un numero ingresado por el usuario')
-  print('4 -> Primeros 100 numeros pares')
+  print('4 -> Primeros 10 numeros pares')
   print('5 -> Numeros pares hasta un numero ingresado por el usuario')
   print('6 -> Factorial de un numero')
   print('7 -> Suma de los digitos de un numero')
+  print('8 -> Multiplos de 3 hasta el numero 100')
+  print('9 -> Numeros impares hasta el numero ingresado por el usuario')
+  print('10 -> Suma de numeros pares del 2 al 100')
   print('0 -> salir')
 
   option = int(input('Selecciona una opcion: '))
@@ -77,6 +103,12 @@ while True:
     factorial()
   elif option == 7:
     addition_digits()
+  elif option == 8:
+    multiply_numbers_three()
+  elif option == 9:
+    odd_numbers_to_user_number()
+  elif option == 10:
+    pair_number_to_one_hundred()
   elif option == 0:
     print('saliendo...')
     break
