7181dbc110c996a8caae6e4f7e52a7f99f84efa0
Victor Castañeda <victorarroyavecastaneda@gmail.com>
2024-04-14 11:04:39 -0500

    update: new exercises

--- logica-de-programacion_1/clase1/exercises/clase1.js
+++ logica-de-programacion_1/clase1/exercises/clase1.js
@@ -23,3 +23,33 @@ console.log(substract(9, 1))
 console.log(multiply(9, 9))
 console.log(divide(4, 2))
 console.log(triangleArea(8, 9))
+
+// de celsius a fahrenheit
+//°F = (°C x 9/5) + 32
+
+function converter( celsius ) {
+  let fahrenheit = ( celsius * 9/5) + 32
+  return fahrenheit
+}
+
+console.log(`${converter(32)} °F`)
+
+//print result
+
+function printResult() {
+  console.log(((8 * 2) / 6) + 4)
+}
+
+printResult()
+
+//notes average
+function gradeAverage(notes) {
+  let addition = 0
+  for (let i = 0; i < notes.length; i++) {
+    addition += notes[i]
+  }
+  return addition / notes.length
+}
+
+let notesList = [4.0, 2.7, 3.2]
+console.log(`El promedio de notas es: ${gradeAverage(notesList)}`)
--- logica-de-programacion_1/clase1/exercises/clase1.ts
+++ logica-de-programacion_1/clase1/exercises/clase1.ts
@@ -15,9 +15,35 @@
   return (base * height) / 2
  }
 
+ // de celsius a fahrenheit
+ //°F = (°C x 9/5) + 32
+ function converter( celsius:number ): number {
+  let fahrenheit = ( celsius * 9/5) + 32
+  return fahrenheit
+ }
+
+ //print result
+function printResult(): void {
+ console.log(((8 * 2) / 6) + 4)
+}
+
+//notes average
+function gradeAverage(notes: Array<number>): number {
+ let addition = 0
+ for (let i = 0; i < notes.length; i++) {
+   addition += notes[i]
+ }
+ return addition / notes.length
+}
+
+let notesList = [4.0, 2.7, 3.2]
+
  console.log(addition(8,9))
  console.log(substract(6,3))
  console.log(multiply(8,9))
  console.log(division(4,2))
  console.log(triangleArea(8, 9))
+ console.log(`${converter(32)} °F`)
+ printResult()
+ console.log(gradeAverage(notesList))
 })()
